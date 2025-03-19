30c735439b0b82505f409185d35f06b85da2ef54
deeptft <singh.deep@tftus.com>
2025-03-19 20:00:52 +0530

    DVC intiated with version 1

--- mycode.py
+++ mycode.py
@@ -8,3 +8,7 @@ import os
 import shutil
 
 shutil.copy("sample_top/1.png", "data")
+
+# dvc init
+# dvc remote add -d myremote s3       # store data versioning here
+# dvc add data                    # you are reposible for versioning insid data folder
